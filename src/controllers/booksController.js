c448649e9fedd2754ea38847a3687744204b8db9
Mateusz Książek <ksiazek.mate@gmail.com>
2017-12-31 16:47:01 +0100

    Fix issue

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -14,14 +14,14 @@ export function getAllBooks() {
 }
 
 export async function addBook(root, {authorId, input}) {
-    const authorId = new mongoose.Types.ObjectId(authorId);
-    const author = await Author.findById(authorId);
+    const authorObjectId = new mongoose.Types.ObjectId(authorId);
+    const author = await Author.findById(authorObjectId);
 
     if (!author) {
         throw new Error('Author is not exists');
     }
 
-    input.author = authorId;
+    input.author = authorObjectId;
     const book = await new Book(input).save();
 
     author.books.push(book);
