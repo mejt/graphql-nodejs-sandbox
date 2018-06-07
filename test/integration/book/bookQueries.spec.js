8ff331718144feeac835d68d68c66725e5b79720
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2018-06-07 22:30:25 +0200

    Add tests for book queries

---
+++ test/integration/book/bookQueries.spec.js
@@ -0,0 +1,98 @@
+'use strict';
+
+import request from 'supertest';
+
+import AppFactory from './../../../src/initServer';
+import configurationProvider from './../../../src/configurationProvider';
+
+const appFactory = AppFactory(configurationProvider);
+
+describe('Book Queries', () => {
+    let app;
+
+    const bookDaoMock = {};
+
+    beforeEach(() => {
+        bookDaoMock.getAll = jest.fn();
+        bookDaoMock.getById = jest.fn();
+
+        app = appFactory({}, bookDaoMock);
+    });
+
+    afterEach(() => {
+        app.close();
+    });
+
+    test('should return correct fields for getBooks query', async () => {
+        bookDaoMock.getAll.mockReturnValue([
+            { id: 1, title: 'Great book', pages: 743, isbn: '3351498751543', description: 'hello world' },
+            { id: 2, title: 'Greater book', pages: 865, isbn: '3351498751546', description: 'HELLO WORLD' }
+        ]);
+
+        const query = `
+            query getBooks {
+              books {
+                title
+                pages
+              }
+            }
+        `;
+
+        const response = await request(app)
+            .post('/')
+            .set('Content-type', 'application/json')
+            .send({ query });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+            data: {
+                books: [
+                    { title: 'Great book', pages: 743 },
+                    { title: 'Greater book', pages: 865 }
+                ]
+            }
+        });
+    });
+
+    test('should return correct fields for getBook query', async () => {
+        bookDaoMock.getById.mockReturnValue({
+            id: 2,
+            title: 'The greatest book',
+            pages: 743,
+            isbn: '3351498751543',
+            description: 'hello world',
+            author: {
+                id: 5, name: 'John Doe',
+                birthday: new Date(1970, 1, 1)
+            }
+        });
+
+        const query = `
+            query getBook {
+              book(id: 2) {
+                title
+                description
+                author {
+                    name
+                }
+              }
+            }
+        `;
+
+        const response = await request(app)
+            .post('/')
+            .set('Content-type', 'application/json')
+            .send({ query });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+            data: {
+                book: {
+                    title: 'The greatest book',
+                    description: 'hello world',
+                    author: { name: 'John Doe' }
+                }
+            }
+        });
+    });
+});
